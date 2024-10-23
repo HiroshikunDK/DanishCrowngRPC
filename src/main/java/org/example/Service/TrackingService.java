f9490766d453944c26a0017b9410892bf9ff98c9
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2024-10-23 22:23:02 +0200

    Implemented new function closer to assignment 2 specification

--- src/main/java/org/example/Service/TrackingService.java
+++ src/main/java/org/example/Service/TrackingService.java
@@ -8,6 +8,7 @@ import org.example.data.DatabaseConnectionLite;
 import via.pro3.grpcspringbootexample.grpc.*;
 import org.example.data.DatabaseConnection;
 
+import java.io.Console;
 import java.sql.*;
 public class TrackingService extends TrackingServiceGrpc.TrackingServiceImplBase {
     private static final String DB_URL = "jdbc:postgresql://localhost:5433/postgres?currentSchema=DanishCrownDB";
@@ -21,11 +22,45 @@ public class TrackingService extends TrackingServiceGrpc.TrackingServiceImplBase
     @Override
     public void getProductsFromAnimal(Animal request, StreamObserver<AniProRegistrationList> responseObserver) {
         AniProRegistrationList.Builder responseBuilder = AniProRegistrationList.newBuilder();
-
+        System.out.println("Animal " + request.getName() + ", is a part of products(Registration Number): ");
         try (Connection conn = dbconn.getConnection();
+
              PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM \"aniproregistration\" WHERE animalregnr = ?")) {
 
-            pstmt.setInt(1, request.getRegNr());  // Assuming reg_nr is used to link animals to registrations
+            pstmt.setInt(1, request.getRegNr());
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                AniProRegistration aniProRegistration = AniProRegistration.newBuilder()
+                        .setId(rs.getInt("ID_"))
+                        .setRegNr(rs.getInt("RegNr"))
+                        .setProductionDate(rs.getString("ProductionDate"))
+                        .setWeight(rs.getFloat("Weight_"))
+                        .setAnimalRegNr(rs.getInt("AnimalRegNr"))
+                        .setProductRegNr(rs.getInt("ProductRegNr"))
+                        .build();
+                responseBuilder.addResultList(aniProRegistration);
+
+                System.out.println(rs.getInt("RegNr") + ", ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();  // Consider logging this instead
+        }
+
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    //Not as relevant solution as AniProRegistration are the internal batchnumbers/registration for the processed meat
+    @Override
+    public void getAnimalFromProduct(Product request, StreamObserver<AniProRegistrationList> responseObserver) {
+        AniProRegistrationList.Builder responseBuilder = AniProRegistrationList.newBuilder();
+        System.out.println("Product " + request.getName() + ", (Registration Number): ");
+
+        try (Connection conn = dbconn.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM \"aniproregistration\" WHERE ProductRegNr = ?")) {
+
+            pstmt.setInt(1, request.getRegNr());  // Using the reg_nr from the Product request
 
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
@@ -46,26 +81,31 @@ public class TrackingService extends TrackingServiceGrpc.TrackingServiceImplBase
         responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
     }
+
     @Override
-    public void getAnimalFromProduct(Product request, StreamObserver<AniProRegistrationList> responseObserver) {
-        AniProRegistrationList.Builder responseBuilder = AniProRegistrationList.newBuilder();
+    public void getAnimalFromProductRegistation(AniProRegistration request, StreamObserver<AnimalList> responseObserver) {
+        AnimalList.Builder responseBuilder = AnimalList.newBuilder();
+        System.out.println("Product registration " + request.getRegNr() + ",contains the following animals: ");
 
         try (Connection conn = dbconn.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM \"aniproregistration\" WHERE ProductRegNr = ?")) {
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM animal WHERE RegNr IN (SELECT AnimalRegNr FROM \"aniproregistration\" WHERE RegNr = ?)")) {
 
-            pstmt.setInt(1, request.getRegNr());  // Using the reg_nr from the Product request
+            pstmt.setInt(1, request.getRegNr());  // Using the reg_nr from the request
 
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
-                AniProRegistration aniProRegistration = AniProRegistration.newBuilder()
+                Animal animalRegistration = Animal.newBuilder()
                         .setId(rs.getInt("ID_"))
                         .setRegNr(rs.getInt("RegNr"))
-                        .setProductionDate(rs.getString("ProductionDate"))
+                        .setName(rs.getString("Name_"))
+                        .setSpecies(rs.getString("Species_"))
+                        .setSubSpecies(rs.getString("SubSpecies"))
+                        .setBirthday(rs.getString("Birthday"))
                         .setWeight(rs.getFloat("Weight_"))
-                        .setAnimalRegNr(rs.getInt("AnimalRegNr"))
-                        .setProductRegNr(rs.getInt("ProductRegNr"))
+                        .setFarmRegNr(rs.getInt("FarmRegNr"))
                         .build();
-                responseBuilder.addResultList(aniProRegistration);
+                responseBuilder.addAnimals(animalRegistration);
+                System.out.println(rs.getString("Name_") + ", ");
             }
         } catch (Exception e) {
             e.printStackTrace();  // Consider logging this instead
